12bf8a5131c6bc1c6dc12affc439b8675afddef7
Tomasz Kurowski <tkurowski@opera.com>
2015-12-09 10:21:21 +0100

    plot2

--- plot2.R
+++ plot2.R
@@ -5,12 +5,13 @@ select <- "select * from file where Date in ('1/2/2007', '2/2/2007')"
 
 # read
 df <- read.csv2.sql('household_power_consumption.txt', sql = select, na.strings = '?')
+# mutate
+df$datetime = strptime(paste(df$Date, df$Time), '%d/%m/%Y %H:%M:%S')
 
-png(file = "plot1.png")
+png(file = "plot2.png")
 
-hist(df$Global_active_power,
-     col = 'red',
-     main = "Global Active Power",
-     xlab = "Global Active Power (kilowatts)")
+plot(df$datetime, df$Global_active_power, type = 'n', xlab = '',
+     ylab = "Global Active Power (kilowatts)")
+lines(df$datetime, df$Global_active_power)
 
 dev.off()
